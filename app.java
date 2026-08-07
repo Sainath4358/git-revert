88f433413a2a6c302e03190452dc6e785228d31b
Sainath4358 <sainathsatrasala2000@gmail.com>
2026-08-07 16:33:02 +0530

    Fix case sensitivity and add print statement

--- app.java
+++ app.java
@@ -1,7 +1,7 @@
 public class App {
 	public static void main(string[] args){
 		system.out.println("Good Morning:")
-                system.out.println("Git Revere command")
+        system.out.println("Git Revere command")
 		system.out.println("New feature added-1")
 
 	}
