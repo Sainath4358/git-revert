fc216a28cc60b6cbe7e116059ede214c97643472
Sainath4358 <sainathsatrasala2000@gmail.com>
2026-08-07 16:34:23 +0530

    Correct print statement syntax in App class
    Fix syntax error in print statement and update message.

--- app.java
+++ app.java
@@ -1,6 +1,6 @@
 public class App {
 	public static void main(string[] args){
-		system.out.println("Good Morning:")
+		system.out.println("Good Morning")
         system.out.println("Git Revere command")
 		system.out.println("New feature added-1")
 
