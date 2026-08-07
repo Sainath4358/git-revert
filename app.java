4e3a51f9967e9a788c837d0cc487681234c74cfe
Sainath4358 <sainathsatrasala2000@gmail.com>
2026-08-07 16:35:03 +0530

    Fix syntax errors in App class

--- app.java
+++ app.java
@@ -1,8 +1,8 @@
 public class App {
 	public static void main(string[] args){
-		system.out.println("Good Morning")
-        system.out.println("Git Revere command")
-		system.out.println("New feature added-1")
+		system.out.println("Good Morning");
+        system.out.println("Git Revere command");
+		system.out.println("New feature added-1");
 
 	}
 }
