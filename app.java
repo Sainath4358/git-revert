4486d1573281a214f253474706d7a946081e0613
Sainath4358 <sainathsatrasala2000@gmail.com>
2026-08-07 16:40:36 +0530

    Fix typo in print statement for Git command

--- app.java
+++ app.java
@@ -1,7 +1,7 @@
 public class App {
 	public static void main(String[] args){
 		System.out.println("Good Morning");
-        System.out.println("Git Revere command");
+        System.out.println("Git Revert command");
 		System.out.println("New feature added-1");
 
 	}
